4d27cc949c77694fdd477162d0745d329296fcd1
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-10-07 16:51:12 +0100

    Updates

---
+++ precache-manifest.d3cceb61360c88e1306ce366ea049cfe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca0914f3ce1f11263a3c7447965bab7",
+    "url": "/postform/index.html"
+  },
+  {
+    "revision": "8d5bd8f60d288bfa488b",
+    "url": "/postform/static/css/2.037c7eed.chunk.css"
+  },
+  {
+    "revision": "0871c00c1e3689f48015",
+    "url": "/postform/static/css/main.af922f3e.chunk.css"
+  },
+  {
+    "revision": "8d5bd8f60d288bfa488b",
+    "url": "/postform/static/js/2.f6b1b228.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/postform/static/js/2.f6b1b228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0871c00c1e3689f48015",
+    "url": "/postform/static/js/main.a3791b44.chunk.js"
+  },
+  {
+    "revision": "4774d63749f3abb40355",
+    "url": "/postform/static/js/runtime-main.04c28363.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/postform/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/postform/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/postform/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/postform/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/postform/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
